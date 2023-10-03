71f42c8d720463a8f791d6a8b1fda0d91e002b0d
Olivier Ory <olivier.ory@uliege.be>
2023-10-03 16:28:08 +0200

    Echo + No New Line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, default_value_t = false)]
+    no_new_line: bool,
+
+    input: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let mut to_print = String::from("");
+    if let Some(input) = args.input {
+        to_print = input;
+    }
+
+    if args.no_new_line {
+        print!("{}", to_print);
+    } else {
+        println!("{}", to_print);
+    }
 }
